9d26b240ce32bcea1aa31dd221bf485e0eefdc78
sabira11 <thasinsabira129@gmail.com>
2024-12-01 14:32:49 +0600

    updated app.py

--- app.py
+++ app.py
@@ -18,13 +18,41 @@ def omr_perspective_show():
     image_path="./images/"+ image_file.filename
     image_file.save(image_path)
     processed_image = omr_sheet_perspective_change.remove_background(image_path)
-    processed_filename = 'scanned_document2.jpg'
+    processed_filename = 'scanned_document3.jpg'
     processed_path = os.path.join(app.config['IMAGE_FOLDER'], processed_filename)
+    
     cv2.imwrite(processed_path, processed_image)
+    score,ans,detect_index=omr_test_grading.grader(processed_path)
+    arr=[]
+    for i in range(1,26):
+        arr.append(i)
+        if detect_index[i-1]==0:
+            detect_index[i-1]='A'
+        elif detect_index[i-1]==1:
+            detect_index[i-1]='B'
+        elif detect_index[i-1]==2:
+            detect_index[i-1]='C'
+        elif detect_index[i-1]==3:
+            detect_index[i-1]='D'
+    for i in range(1,26):
+        if ans[i]==1:
+            ans[i]='A'
+        elif ans[i]==2:
+            ans[i]='B'
+        elif ans[i]==3:
+            ans[i]='C'
+        elif ans[i]==4:
+            ans[i]='D'
+    for i in range (1,len(detect_index)+1):
+       if ans[i]==' ':
+         detect_index[i-1]=' '
+    
+    
+    prediction = list(zip(arr, detect_index))
    
     
 
-    return render_template('omr_upload.html',processed_image=processed_filename)
+    return render_template('omr_upload.html',processed_image=processed_filename,prediction=prediction,index=ans)
 @app.route('/static/saved_image/<filename>')
 def serve_image(filename):
     return send_from_directory(app.config['IMAGE_FOLDER'], filename)
